19dede77109415c72a50d8f868bc54b1dc4dc2f3
515291 <515291@chrome.escg.ac.uk>
2020-11-10 12:10:27 +0000

    Started placer controller

--- Assets/Scripts/vPlayerController.cs
+++ Assets/Scripts/vPlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class vPlayerController : MonoBehaviour
 {
+
+    public float moveSpeed = 9f;
+    public Transform movePoint;
     // Start is called before the first frame update
     void Start()
     {
