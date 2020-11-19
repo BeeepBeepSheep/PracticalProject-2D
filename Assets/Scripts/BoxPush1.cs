2c015969c16906a70757742dbaa8cd99d52731f6
515291 <515291@chrome.escg.ac.uk>
2020-11-19 11:34:45 +0000

    Got box push increment working

--- Assets/Scripts/BoxPush1.cs
+++ Assets/Scripts/BoxPush1.cs
@@ -17,7 +17,7 @@ public class BoxPush1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Hit before");
+        Debug.Log("box detected and should move");
         transform.position = Vector3.MoveTowards(transform.position, boxMovePoint.position, moveSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, boxMovePoint.position) <= .05f)
@@ -30,7 +30,7 @@ public class BoxPush1 : MonoBehaviour
                     boxMovePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
-            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f); //else if make it so box doesent go diagonally
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) ; //else if make it so box doesent go diagonally
             {
                 if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .5f, WhatStopsBoxMovement))
                 {
@@ -38,7 +38,5 @@ public class BoxPush1 : MonoBehaviour
                 }
             }
         }
-        Debug.Log("Hit after");
-
     }
 }
