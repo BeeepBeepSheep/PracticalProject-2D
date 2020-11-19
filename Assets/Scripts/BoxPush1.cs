7be3a3fecb1f27fafbe2e5fd9054896bc4232d43
515291 <515291@chrome.escg.ac.uk>
2020-11-19 12:17:03 +0000

    got horizontal movement

--- Assets/Scripts/BoxPush1.cs
+++ Assets/Scripts/BoxPush1.cs
@@ -20,23 +20,6 @@ public class BoxPush1 : MonoBehaviour
         Debug.Log("box detected and should move");
         transform.position = Vector3.MoveTowards(transform.position, boxMovePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, boxMovePoint.position) <= .05f)
-        {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                //checking if theres colliders
-                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, WhatStopsBoxMovement))
-                {
-                    boxMovePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
-            }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) ; //else if make it so box doesent go diagonally
-            {
-                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .5f, WhatStopsBoxMovement))
-                {
-                    boxMovePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                }
-            }
-        }
+      
     }
 }
