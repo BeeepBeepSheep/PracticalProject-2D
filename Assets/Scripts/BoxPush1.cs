3354b82219988167e4bc29b3313c0389f595f77c
515291 <515291@chrome.escg.ac.uk>
2020-11-19 10:37:05 +0000

    fixed edges

--- Assets/Scripts/BoxPush1.cs
+++ Assets/Scripts/BoxPush1.cs
@@ -7,7 +7,7 @@ public class BoxPush1 : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform boxMovePoint;
 
-    public LayerMask whatStopsMovement;
+    public LayerMask WhatStopsBoxMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +25,14 @@ public class BoxPush1 : MonoBehaviour
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
                 //checking if theres colliders
-                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, WhatStopsBoxMovement))
                 {
                     boxMovePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
-            else (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f); //else if make it so box doesent go diagonally
             {
-                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .5f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(boxMovePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .5f, WhatStopsBoxMovement))
                 {
                     boxMovePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
