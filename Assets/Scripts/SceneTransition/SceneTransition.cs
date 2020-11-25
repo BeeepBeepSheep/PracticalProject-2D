904b33ba589cd50eec983824267124aafda894e3
DESKTOP-PNBCHAE\jacob <jacoblueke@gmail.com>
2020-11-25 14:23:28 +0000

    Added scene controller script to finish collider (need to make it so only i scene changer per level)

---
+++ Assets/Scripts/SceneTransition/SceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManegement;
+public class SceneTransition : MonoBehaviour
+{
+    private int nextSceneToLoad;
+    void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    void OnTriggerEnter2D(Colldier2D collision)
+    {
+        SceneManager.LoadScene(nextSceneToLoad);
+    }
+}
