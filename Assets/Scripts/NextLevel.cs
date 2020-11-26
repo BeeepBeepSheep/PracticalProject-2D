c78ab95a8ed3b368dd0937a3f88f2d8721f5534f
515291 <515291@chrome.escg.ac.uk>
2020-11-26 11:28:26 +0000

    still broken

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadNext : MonoBehaviour
+public class NextLevel : MonoBehaviour
 {
     private int nextSceneToLoad;
     void Start()
