0f002b50656eeb2dce68cb4535dd6635d0c0f6d9
515291 <515291@chrome.escg.ac.uk>
2020-12-08 10:47:21 +0000

    got it so in level 2 the audio pauses with pause

---
+++ Assets/Scripts/AudioPause.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void OnClickPause()
+    {
+        AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+}
