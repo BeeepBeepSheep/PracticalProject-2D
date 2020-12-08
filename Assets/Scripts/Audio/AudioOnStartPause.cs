193993f88e7c0e4977e98fcd98a8bae2ece6164a
515291 <515291@chrome.escg.ac.uk>
2020-12-08 11:25:02 +0000

    Got pause music working fully (need to stop it in menu)

---
+++ Assets/Scripts/Audio/AudioOnStartPause.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AusioPauseOnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+}
+
+
--- Assets/Scripts/Audio/AudioPause.cs
+++ Assets/Scripts/Audio/AudioPause.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class AudioPause : MonoBehaviour
 {
-    // Start is called before the first frame update
+
 
     public void OnClickPause()
     {
@@ -16,10 +16,16 @@ public class AudioPause : MonoBehaviour
     }
     public void isSceneMenu()
     {
-        if (SceneManager.GetActiveScene().buildIndex == "Menu")
+        if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Pause();
         }
+        else //(SceneManager.GetActiveScene().buildIndex != 1)
+        {
+            AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Play();
+        }
+
+     
     }
 
 }
