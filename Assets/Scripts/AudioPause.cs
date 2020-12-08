ebeea87cf91ddbcb1943c281e85856a3f1d71af8
515291 <515291@chrome.escg.ac.uk>
2020-12-08 11:02:55 +0000

    no message

--- Assets/Scripts/AudioPause.cs
+++ Assets/Scripts/AudioPause.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class AudioPause : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -10,4 +10,18 @@ public class AudioPause : MonoBehaviour
     {
         AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Pause();
     }
+    public void OnClickPlay()
+    {
+        AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Play();
+    }
+    public void isSceneMenu()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == "Menu")
+        {
+            AudioBetweenScenes.Instance.gameObject.GetComponent<AudioSource>().Pause();
+        }
+    }
+
 }
+
+
